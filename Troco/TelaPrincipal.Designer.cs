2e5d38177e4cadf401d0b6e27f47c7ba31c2aa12
Jonathan Previtalli <jonathan.previtalli@gmail.com>
2021-09-22 12:03:07 -0300

    Adicionar arquivos de projeto.

---
+++ Troco/TelaPrincipal.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace Troco
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCompra = new System.Windows.Forms.TextBox();
+            this.txtDinheiro = new System.Windows.Forms.TextBox();
+            this.txtTroco = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label100 = new System.Windows.Forms.Label();
+            this.label50 = new System.Windows.Forms.Label();
+            this.label025 = new System.Windows.Forms.Label();
+            this.label010 = new System.Windows.Forms.Label();
+            this.label005 = new System.Windows.Forms.Label();
+            this.label001 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblTitulo.Font = new System.Drawing.Font("Anton", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(294, 53);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(240, 52);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Caixa para Troco";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(239, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 113);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ICEBOLD", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor da Compra:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("ICEBOLD", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(232, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Valor em Dinheiro:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("ICEBOLD", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(63, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Troco:";
+            // 
+            // txtCompra
+            // 
+            this.txtCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCompra.Location = new System.Drawing.Point(340, 178);
+            this.txtCompra.Name = "txtCompra";
+            this.txtCompra.Size = new System.Drawing.Size(182, 28);
+            this.txtCompra.TabIndex = 5;
+            this.txtCompra.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDinheiro
+            // 
+            this.txtDinheiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDinheiro.Location = new System.Drawing.Point(340, 229);
+            this.txtDinheiro.Name = "txtDinheiro";
+            this.txtDinheiro.Size = new System.Drawing.Size(182, 28);
+            this.txtDinheiro.TabIndex = 6;
+            this.txtDinheiro.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTroco
+            // 
+            this.txtTroco.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTroco.Location = new System.Drawing.Point(340, 272);
+            this.txtTroco.Name = "txtTroco";
+            this.txtTroco.ReadOnly = true;
+            this.txtTroco.Size = new System.Drawing.Size(182, 28);
+            this.txtTroco.TabIndex = 7;
+            this.txtTroco.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.Black;
+            this.btnCalcular.Location = new System.Drawing.Point(328, 351);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(206, 64);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(694, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Moedas R$ 1,00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(694, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Moedas R$ 0,50";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(694, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Moedas R$ 0,25";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(694, 273);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Moedas R$ 0,10";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(694, 306);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 20);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Moedas R$ 0,05";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(694, 337);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(130, 20);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Moedas R$ 0,01";
+            // 
+            // label100
+            // 
+            this.label100.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label100.Location = new System.Drawing.Point(623, 168);
+            this.label100.Name = "label100";
+            this.label100.Size = new System.Drawing.Size(54, 17);
+            this.label100.TabIndex = 15;
+            // 
+            // label50
+            // 
+            this.label50.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label50.Location = new System.Drawing.Point(623, 203);
+            this.label50.Name = "label50";
+            this.label50.Size = new System.Drawing.Size(54, 17);
+            this.label50.TabIndex = 16;
+            // 
+            // label025
+            // 
+            this.label025.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label025.Location = new System.Drawing.Point(623, 240);
+            this.label025.Name = "label025";
+            this.label025.Size = new System.Drawing.Size(54, 17);
+            this.label025.TabIndex = 17;
+            // 
+            // label010
+            // 
+            this.label010.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label010.Location = new System.Drawing.Point(623, 271);
+            this.label010.Name = "label010";
+            this.label010.Size = new System.Drawing.Size(54, 17);
+            this.label010.TabIndex = 18;
+            // 
+            // label005
+            // 
+            this.label005.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label005.Location = new System.Drawing.Point(623, 306);
+            this.label005.Name = "label005";
+            this.label005.Size = new System.Drawing.Size(54, 17);
+            this.label005.TabIndex = 19;
+            // 
+            // label001
+            // 
+            this.label001.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label001.Location = new System.Drawing.Point(623, 336);
+            this.label001.Name = "label001";
+            this.label001.Size = new System.Drawing.Size(54, 17);
+            this.label001.TabIndex = 20;
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 427);
+            this.Controls.Add(this.label001);
+            this.Controls.Add(this.label005);
+            this.Controls.Add(this.label010);
+            this.Controls.Add(this.label025);
+            this.Controls.Add(this.label50);
+            this.Controls.Add(this.label100);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtTroco);
+            this.Controls.Add(this.txtDinheiro);
+            this.Controls.Add(this.txtCompra);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "TelaPrincipal";
+            this.ShowIcon = false;
+            this.Text = "Caixa :: Troco";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCompra;
+        private System.Windows.Forms.TextBox txtDinheiro;
+        private System.Windows.Forms.TextBox txtTroco;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label100;
+        private System.Windows.Forms.Label label50;
+        private System.Windows.Forms.Label label025;
+        private System.Windows.Forms.Label label010;
+        private System.Windows.Forms.Label label005;
+        private System.Windows.Forms.Label label001;
+    }
+}
+
